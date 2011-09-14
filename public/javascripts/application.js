dab15442f0cff239e3bab54e8d6c990d629df7d7
Jake Bell <jake@theunraveler.com>
2011-09-14 10:48:43 -0500

    Needs a little tweaking for style, but functions well.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
 	// Other behaviors.
 	choreFormConditional();
+	choreFormDifficultySlider();
 
 });
 
@@ -25,6 +26,34 @@ var choreFormConditional = function() {
 		} else {
 			dayContainer.hide();
 			dayContainer.find('input').attr('checked', false);
+		}
+	});
+};
+
+/**
+ * Add a jQuery UI slider to the difficulty field.
+ */
+var choreFormDifficultySlider = function() {
+	var difficultyTextOptions = {
+		1: 'Easy',
+		2: 'Meh.',
+		3: 'Kind of hard',
+		4: 'Hard',
+		5: 'Really hard'
+	}
+	var difficultyText = $('#difficulty-text');
+	var difficultyField = $('input[name="chore[difficulty]"]');
+	difficultyText.text(difficultyTextOptions[difficultyField.val() || 1]);
+
+	$('#difficulty-slider').slider({
+		min: 1,
+		max: 5,
+		value: difficultyField.val() || 1,
+		slide: function(event, ui) {
+			difficultyText.text(difficultyTextOptions[ui.value]);
+		},
+		change: function(event, ui) {
+			difficultyField.val(ui.value);
 		}
 	});
 };
