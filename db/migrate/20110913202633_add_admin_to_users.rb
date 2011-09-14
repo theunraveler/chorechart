b091d22049c01a41ed3ed57de1bf61bcc8d5cc6f
Jake Bell <jake@theunraveler.com>
2011-09-14 14:06:24 -0500

    Fixing issue with admin migration.

--- db/migrate/20110913202633_add_admin_to_users.rb
+++ db/migrate/20110913202633_add_admin_to_users.rb
@@ -1,6 +1,6 @@
 class AddAdminToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :is_admin, :boolean, :default => 0, :after => :email
+    add_column :users, :is_admin, :boolean, :default => false, :after => :email
   end
 
   def self.down
