c58e27d61481aab1e805328b1d6e208b3480cc4a
Jake Bell <jake@theunraveler.com>
2011-10-15 01:56:35 -0500

    Error in user chore sidebar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :get_current_user_chores
+
+  def get_current_user_chores
+    if user_signed_in?
+      @user_chores = current_user.assignments_for(Date.today, Date.today.advance(:days => 4)).group_by(&:date)
+    end
+  end
 end
