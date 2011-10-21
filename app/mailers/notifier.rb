5dda07ee6748c2b980bba0048cd519ba7fcc13b3
Jake Bell <jake@theunraveler.com>
2011-10-20 20:04:07 -0500

    Adding sender to mailer.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
   def invite(email, group, inviter)
     @group = group
     @inviter = inviter
-    mail(:to => email, :subject => "Join Chorechart!")
+    mail(:to => email, :subject => "Join Chorechart!", :from => 'info@chorechartapp.com')
   end
 
 end
