2ed227f5c64d0caa2cb20e3a707cbb796d211abf
Jake Bell <jake@theunraveler.com>
2011-10-31 15:04:17 -0500

    returning is now Object.tap

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -3,7 +3,7 @@ module GroupsHelper
   def user_points_for_week(user, week)
     current_week = (week..week.end_of_week)
 
-    returning [] do |total|
+    [].tap do |total|
       user.assignments.select { |a| current_week.include?(a.date) }.each do |assignment|
         total << assignment.chore.difficulty
       end
