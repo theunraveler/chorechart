c885dcbe18ef89e3b8434355d940407b022f72db
Jake Bell <jake@theunraveler.com>
2011-11-03 17:55:29 -0500

    Trying to decrease slug size.

--- config/initializers/escape_utils.rb
+++
@@ -1,9 +0,0 @@
-# This is to fix error when rack encounters URL queries w/ UTF-8
-# Found here: http://crimpycode.brennonbortz.com/?p=42
-module Rack
-  module Utils
-    def escape(s)
-      EscapeUtils.escape_url(s)
-    end
-  end
-end
