3336f8335e593db4021a7452548afbc0339cad8c
Jake Bell <jake@theunraveler.com>
2011-11-17 00:54:22 -0600

    Styles for features page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   respond_to :html
   layout 'no_sidebar'
   skip_before_filter :authenticate_user!
+  caches_page :faq, :features
 
   def index
     if user_signed_in?
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,7 +4,7 @@ module PagesHelper
     options = args.extract_options!
     options[:rel] = 'lightbox'
     thumb_path = path.gsub('.', '-thumb.')
-    link_to image_tag(thumb_path), image_path(path), options
+    link_to image_tag(thumb_path, :class => 'bordered'), image_path(path), options
   end
 
 end
