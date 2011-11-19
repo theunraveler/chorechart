dba45788370e2d5ecac67ac1ad82afa425affc5f
Jake Bell <jake@theunraveler.com>
2011-11-19 02:10:55 -0600

    Refactored considerably, but getting a weird error on initial week display after assignment generation. Pretty sure its in the view.

--- app/models/group.rb
+++ app/models/group.rb
@@ -13,33 +13,34 @@ class Group < ActiveRecord::Base
 
   # Get a list of chores for the day
   def assignments_for(start = Time.current.to_date, finish = Time.current.to_date)
-    assigns = assignments.find_all_by_date(start..finish)
-    if assigns.empty? && chore_occurrences_between(start, finish).any?
-      Assigner.create_schedule_for(self, start, finish)
-      # Reload data from the DB so we don't get stale data
-      reload
-      assigns = assignments_for(start, finish)
+    [].tap do |assigns|
+      assigns.concat find_assignments_by_date(start, finish)
+      if assigns.empty? && chore_occurrences_between(start, finish).any?
+        Assigner.create_schedule_for(self, start, finish)
+        assigns.concat find_assignments_by_date(start, finish, true)
+      end
     end
-    return assigns
+  end
+
+  def find_assignments_by_date(start, finish, clear = false)
+    ActiveRecord::Base.clear_cache! if clear 
+    assignments.find_all_by_date(start..finish)
   end
 
   # Get a user's workload for a certain week
   def workload(user, week)
-    current_week = (week.beginning_of_week..week.end_of_week)
-    assignments.select { |a| a.user == user && current_week.include?(a.date) }.collect(&:difficulty).sum
+    current_week = week.beginning_of_week..week.end_of_week
+    assignments.where(:user_id => user.id, :date => current_week).sum(:difficulty).to_i
   end
 
   # Get chore occurrences within date range
   # TODO: Still kind of ugly.
   def chore_occurrences_between(start, finish)
+    start, finish = start.to_time, finish.advance(:days => 1).to_time
     [].tap do |occurrences|
       chores.each do |chore|
-        chore.schedule.occurrences_between(start.to_time, finish.advance(:days => 1).to_time).each do |occurrence|
-          occurrences << {
-            :date => occurrence.to_date,
-            :chore => chore
-          }
-        end
+        items = chore.schedule.occurrences_between(start, finish)
+        occurrences.concat items.collect { |i| { :date => i.to_date, :chore => chore } }
       end
     end
   end
